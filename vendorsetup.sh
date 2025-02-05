40b4ed10f64b6c6fc7235c5d53400f8efd6a21f6
afterallafk <shivamatiet2001@gmail.com>
2025-02-05 15:23:23 +0530

    RMX2001L1: Setup signing builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,18 @@
+echo 'Starting to sign the build'
+#!/bin/bash
+# Check if the key generation script has been executed before
+KEY_GEN_FLAG=".key_gen_done"
+if [ ! -f $KEY_GEN_FLAG ]; then
+    echo "Downloading and generating keys..."
+    curl -O https://raw.githubusercontent.com/ofcsayan/Key-Gen-signed-script/main/generate_all_keys.sh
+    chmod +x generate_all_keys.sh
+    ./generate_all_keys.sh
+    # Create the flag file to indicate script has run
+    touch $KEY_GEN_FLAG
+else
+    echo "Key generation already completed. Skipping..."
+fi
+
 echo 'Starting to clone stuffs needed for your device'
 
 echo 'Cloning RM6785-common Device tree [1/5]'
